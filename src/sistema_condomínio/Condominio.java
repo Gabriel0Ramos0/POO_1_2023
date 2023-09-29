be9acda4e8636f10d9fa88b4ea19304100a3dbd1
Gabriel0Ramos0 <gabrielrcorrea@outlook.com>
2023-09-29 18:31:01 -0300

    Incompleto 1

--- src/sistema_condomínio/Condominio.java
+++ src/sistema_condomínio/Condominio.java
@@ -1,5 +1,21 @@
 package sistema_condomínio;
 
-public class Condominio {
+import javax.swing.JOptionPane;
 
+public class Condominio {
+	
+	private String apartamento;
+	
+	public void cadastraA() {
+		setApartamento(JOptionPane.showInputDialog(null, "Informe o apartamento"));
+	}
+
+	public String getApartamento() {
+		return apartamento;
+	}
+
+	public void setApartamento(String apartamento) {
+		this.apartamento = apartamento;
+	}
+	
 }
